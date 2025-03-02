f3777273cf0683251a169a1669c192ccaa5ad0cc
Jahanzaib Tayyab <jtayyab204@gmail.com>
2025-03-02 15:11:44 +0500

    add numpay and draw bar chart

--- src/streamlit_github_desktop/main.py
+++ src/streamlit_github_desktop/main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import streamlit as st
 
 st.header("This is 2nd March Header File")
@@ -5,3 +6,8 @@ st.text("Write text without Markdown or HTML parsing.")
 st.text_area("Display a multi-line text input widget.")
 st.text_input("Display a single-line text input widget.")
 st.video("https://youtu.be/KRHg6yZw3r0?si=hetOp5AVkIgLd8zA")
+
+
+message = st.chat_message("assistant")
+message.write("Hello human")
+message.bar_chart(np.random.randn(30, 3))
